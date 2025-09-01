9040929339cc18730b0ea4032ceb6116012a9946
deepesh Agrawal <144640975+deepeshAg@users.noreply.github.com>
2025-09-01 20:39:15 +0530

    Posthog (#89)
    * update the posthog event capture to email id
    
    * initialize the posthog in useEffect

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,11 @@
 export const plivoRate = 0.70;
+
+export const blacklistedEmails = [
+  "da76780@gmail.com",
+  "deepesh@pypeai.com",
+  "adarsh@pypeai.com",
+  "ashish@pypeai.com",
+  "dhruv@pypeai.com",
+  "suryadipta@pypeai.com",
+  "abhinav@pypeai.com",
+];
